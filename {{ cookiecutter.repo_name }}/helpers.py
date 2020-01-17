35266109935613947c556607f472d5e0ea73fade
Baker Renneckar <bakerrenneckar@gmail.com>
2020-01-17 13:37:12 -0800

    added support for multiple reports ; name shortening ; envexample

--- {{ cookiecutter.repo_name }}/helpers.py
+++ {{ cookiecutter.repo_name }}/helpers.py
@@ -11,8 +11,37 @@ from textwrap import fill
 
 
 def load_report(report_id, sf):
-    report = sf.get_report(report_id)
-    parser = ReportParser(report)
+    report_details = sf.get_report(report_id)
+    parser = ReportParser(report_details)
     report = parser.records_dict()
-    report = pd.DataFrame(report)
-    return report
+    df = pd.DataFrame(report)
+
+    while report_details["allData"] == False:
+        existing_ids = ",".join(list(df["18 Digit ID"]))
+        reportFilter = [
+            {
+                "value": f"{existing_ids}",
+                "operator": "notEqual",
+                "column": "Contact.X18_Digit_ID__c",
+            }
+        ]
+        report_details = sf.get_report(report_id, filters=reportFilter)
+        _parser = ReportParser(report_details)
+        _report = _parser.records_dict()
+        _df = pd.DataFrame(_report)
+        df = df.append(_df, ignore_index=True)
+
+    return df
+
+
+def shorten_site_names(df):
+    df.loc[:,'Site'] = df.loc[:,'Site'].str.replace('College Track ', "")
+    df.loc[:,'Site'] = df.loc[:,'Site'].str.replace('at ', "")
+    
+    return df 
+
+def shorten_region_names(df):
+    df.loc[:,'Region'] = df.loc[:,'Region'].str.replace('College Track ', "")
+    df.loc[:,'Region'] = df.loc[:,'Region'].str.replace(' Region', "")
+    
+    return df
