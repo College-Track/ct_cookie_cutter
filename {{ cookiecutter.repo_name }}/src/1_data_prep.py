c59c0d7c17ab5cc39acc6e98a4bddbebfc0750ae
Baker Renneckar <bakerrenneckar@gmail.com>
2021-02-12 16:26:15 -0800

    minor modification to deal with ct_snippet update

--- {{ cookiecutter.repo_name }}/src/1_data_prep.py
+++ {{ cookiecutter.repo_name }}/src/1_data_prep.py
@@ -3,7 +3,12 @@ from simple_salesforce import Salesforce, format_soql
 import os
 from dotenv import load_dotenv
 from ct_snippets.load_sf_class import SF_SOQL, SF_Report
-from ct_snippets.sf_bulk import sf_bulk, sf_bulk_handler, generate_data_dict
+from ct_snippets.sf_bulk import (
+    sf_bulk,
+    sf_bulk_handler,
+    generate_data_dict,
+    process_bulk_results,
+)
 from reportforce import Reportforce
 import numpy as np
 import soql_queries as soql
@@ -25,8 +30,17 @@ rf = Reportforce(session_id=sf.session_id, instance_url=sf.sf_instance)
 # query = """ """
 # data = SF_SOQL("data", query)
 # data.load_from_sf_soql(sf)
+# data.write_file()
 
 
 # Sample for saving data
 # data.write_file(subfolder="interim", file_type=".pkl")
+
+
+# Sample for pushing data to SFDC
+# data_dict = {"C_Id": "Id"}
+
+# data = generate_data_dict(data.df, data_dict)
+# results_df = sf_bulk("Contact", data, sf, batch_size=50, use_serial=True)
+# fail_df = process_bulk_results(results_df, data.df)
 
